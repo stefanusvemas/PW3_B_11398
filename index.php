c0fa366b7c29dc0fd730da2e8ef1db343e289e77
stefanusvemas <stefanusvemas@gmail.com>
2023-09-18 00:51:56 +0700

    Index.php done

--- index.php
+++ index.php
@@ -9,9 +9,9 @@ $detail = [
 ];
 
 $gambar = [
-    ".assets/images/hotel1.jpg",
-    ".assets/images/hotel2.jpg",
-    ".assets/images/hotel3.jpg"
+    "./assets/images/hotel1.jpg",
+    "./assets/images/hotel2.jpg",
+    "./assets/images/hotel3.jpg"
 ];
 
 ?>
@@ -24,10 +24,12 @@ $gambar = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $detail['page_title']; ?></title>
 
+    <link rel="icon" href="<?php echo $detail["logo"]; ?>" type="image/x-icon" />
+
     <link rel="stylesheet" href="./assets/css/bootstrap.min.css" />
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com">
-    <link rel="stylesheet" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="./assets/css/poppins.min.css" rel="stylesheet">
 
     <link rel="stylesheet" href="./assets/css/style.css" />
@@ -68,14 +70,59 @@ $gambar = [
 
             <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visuallyy-hidden">Previous</span>
+                <span class="visually-hidden">Previous</span>
             </button>
             <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
         </div>
+
+        <div class="container">
+            <div class="row featurette">
+                <div class="col-md-7">
+                    <h2 class="featurette-heading fw-normal">
+                        Hotel pertama dan satu-satunya <strong>yang fiksional</strong>
+                    </h2>
+                    <p class="lead">
+                        Diciptakan oleh <strong>Stefanus Vemas aditya Mahardika</strong>,
+                        mahasiswa Universitas Atma Jaya Yogyakarta dari program studi Informatika.
+                    </p>
+                    <p class="lead">Nomor Pokok Mahasiswa <strong>210711398</strong></p>
+                </div>
+                <div class="col-md-5">
+                    <img src="./assets/images/featurette-1.webp" class="featurette-image img-fluid mx-auto rounded shadow" role="img" aria-label="Gambar featurette 1" focusable="false">
+                </div>
+            </div>
+
+            <hr class="featurette-divider">
+
+            <div class="row featurette">
+                <div class="col-md-7 order-md-2">
+                    <h2 class="featurette-heading fw-normal">
+                        Your comfort is key, <strong>experience the heartbeat of our hotel</strong>
+                    </h2>
+                    <p class="lead">
+                        Our modern, sophisticated guest room are designed to exceed expectations with premium comforts
+                        technology where you need it, and thoughtful attention to detail.
+                    </p>
+                </div>
+                <div class="col-md-5 order-md-1">
+                    <img src="./assets/images/featurette-2.jpeg" class="featurette-image img-fluid mx-auto rounded shadow" role="img" aria-label="Gambar featurette 2" foucsable="false">
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <div class="col-md-4 d-flex align-item-center">
+                    <span class="mb-3 mb-md-0 text-body-secondary">©️ 2023 <?php echo $detail["name"]; ?></span>
+                </div>
+            </footer>
+        </div>
     </main>
+    <script src="./assets/css/bootstrap.min.css"></script>
+    <script src="./assets/js/home-nav.js"></script>
 </body>
 
 </html>
