302a49be037887eee5b3652e0cc80ff9916806d0
stefanusvemas <stefanusvemas@gmail.com>
2023-09-18 18:42:28 +0700

    processLogout Done

---
+++ processLogout.php
@@ -0,0 +1,9 @@
+<?php 
+session_start();
+
+if(isset($_SESSION["user"]["bukti_ngantor"])){
+    unlink($_SESSION["user"]["bukti_ngantor"]);
+}
+
+session_destroy();
+header("Loaction: ./login.php");
