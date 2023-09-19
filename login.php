89aedf76237cc8565b46b575b9e9659be467846b
stefanusvemas <stefanusvemas@gmail.com>
2023-09-19 07:21:40 +0700

    Fix style

--- login.php
+++ login.php
@@ -19,26 +19,33 @@ $detail = [
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $detail['page_title']; ?></title>
 
-    <link rel="icon" href="<?php echo $detail["logo"]; ?>" type="image/x-icon" />
-
+    <title><?php echo $detail["page title"]; ?></title>
+    <link rel="icon" href="<?php echo $detail[" logo"]; ?>" type="image/x-icon" />
     <link rel="stylesheet" href="./assets/css/bootstrap.min.css" />
 
+    <!-- Poppins dari Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="./assets/css/poppins.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https: //fonts.gstatic.com" crossorigin>
+    <link href=". /assets/css/poppins.min.css" rel="stylesheet">
 
+    <!-- Custom CSS -->
     <link rel="stylesheet" href="./assets/css/style.css" />
+    <style>
+        #formAuth {
+            max-width: 576px;
+            margin: 0 auto;
+        }
+    </style>
 </head>
 
 <body>
-    <header class="fixed-top" id="navbar">
+<header class="fixed-top scrolled" id="navbar">
         <nav class="container nav-top py-2">
-            <a href="./" class="rounded bg-white py-2 px-3 d-flex align-item-center nav-home-btn">
-                <img src="<?php echo $detail["logo"]; ?>" class="crown-logo">
+            <a href="./" class="rounded bg-white py-2 px-3 d-flex align-items-center nav-home-btn">
+                <img src="<?php echo $detail["logo"]; ?>" class="crown-logo" />
                 <div>
                     <p class="mb-0 fs-5 fw-bold"><?php echo $detail["name"]; ?></p>
                     <p class="small mb-0"><?php echo $detail["tagline"]; ?></p>
@@ -47,51 +54,59 @@ $detail = [
 
             <ul class="nav nav-pills">
                 <li class="nav-item"><a href="./" class="nav-link">Home</a></li>
-                <li class="nav-item"><a href="#" class="nav-link active" area-current="page">Admin Login</a></li>
+                <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Admin Login</a></li>
             </ul>
         </nav>
     </header>
 
-    <main style="padding-top: 84px; margin-bottom: 20px;" class="container">
-        <h1 class="text-center mt-5 display-4">Welcome Admin</h1>
-        <p class="text-center lead">Untuk memastikan identitas, silahkan isi form berikut</p>
 
-        <hr class="featurette-divider">
+    <main style="padding-top : 84px;" class="container">
+        <h1 class="text-center mt-5 display-4">Welcome Admin!</h1>
+        <p class="text-center lead">Untuk memestikan identitas, silahkan isi form berikut:</p>
 
+        <hr class="featurette-divider" />
         <form action="./processLogin.php" method="POST" id="formAuth" enctype="multipart/form-data">
+
+            <!-- https://getbootstrap.com -->
             <div class="alert alert-info mb-4 text-center" role="alert">
-                <strong>Info!</strong> Username dan password bebas, yang penting diisi.
+                <strong>Info!</strong>Username dan Password bebas, yang penting diisi.
             </div>
 
-            <?php if (isset($_SESSION['error'])) { ?>
+            <!-- penggunaan $_SESSION -->
+            <?php if (isset($_SESSION["error"])) { ?>
                 <div class="alert alert-danger mb-4 text-center" role="alert">
-                    <strong>Error</strong> <?php echo $_SESSION["error"]; ?>
+                    <strong>Error!</strong> <? echo $_SESSION["error"]; ?>
                 </div>
             <?php
                 unset($_SESSION["error"]);
             } ?>
 
+            <!-- https://getbootstrap.com -->
             <div class="form-floating mb-4">
-                <input type="text" class="form-control" id="inputUsername" placeholder="Username" name="username">
+                <input type="text" class="form-control" name="username" id="inputUsername" placeholder="Username">
                 <label for="inputUsername">Username</label>
             </div>
             <div class="form-floating mb-4">
-                <input type="password" class="form-control" id="inputPassword" placeholder="Password" name="password">
+                <input type="password" class="form-control" id="inputPassword" placeholder="Password" name="password" />
                 <label for="inputPassword">Password</label>
             </div>
 
-            <div class="mb-4">
-                <label for="inputFile" class="form-label d-block text-center">Bukti sedang ngantor</label>
-                <input class="form-control" id="inputFile" type="file" accept=".jpg,.jpeg,.png" name="bukti_ngantor">
+            <!-- https://getbootstrap.com -->
+            <div>
+                <label for="inputFile" class="form-label d-block text-center">Bukti sedang ngantor:</label>
+                <input type="file" class="form-control" id="inputFile" accept=".jpg,.jpeg,.png name" name="bukti_ngantor" />
+
             </div>
 
             <div>
-                <button type="submit" class="btn btn-primary u-100 fw-bold">Login</button>
+                <button type="submit" class="btn btn-primary w-100 fw-bold">Login</button>
+
+                <!-- kita mengirimkan -->
                 <input type="hidden" name="mencoba_login" value="1">
             </div>
         </form>
-
     </main>
+    <!-- Bootstrap 5.3 JS -->
     <script src="./assets/js/bootstrap.min.js"></script>
 </body>
 
